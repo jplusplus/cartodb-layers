182fb07ed43b2c3268979f121f6cf1021c295320
pirhoo <pierre.romera@gmail.com>
2015-03-24 19:18:34 +0100

    Added basic tests files

---
+++ test/auth.js
@@ -0,0 +1,9 @@
+'use strict';
+var assert = require('assert');
+var cartodbLayers = require('../');
+
+describe('cartodb-layers authentication', function () {
+  it('must have successfully authenticated the user', function () {
+    assert(true);
+  });
+});
---
+++ test/viz.js
@@ -0,0 +1,9 @@
+'use strict';
+var assert = require('assert');
+var cartodbLayers = require('../');
+
+describe('cartodb-layers authentication', function () {
+  it('must have successfully listed the user\'s viz', function () {
+    assert(true);
+  });
+});
