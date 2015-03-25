fc602b9b0e6b4ca2b3c09f31c5bda2c4d605464d
pirhoo <pierre.romera@gmail.com>
2015-03-25 15:05:43 +0100

    First rest test

--- test/rest.js
+++ test/rest.js
@@ -1,10 +1,18 @@
 'use strict';
-//var assert = require('assert');
+var    assert = require('assert'),
+CartodbLayers = require('../'),
+       secret = require('./secret');
 
 describe('CartoDB REST client', function () {
+  // Use the given crediential
+  var cl = new CartodbLayers({ user: secret.USER, api_key: secret.API_KEY });
 
-  it('must reach per-user CartoDB REST API', function () {
-
+  it('must reach per-user CartoDB REST API', function (done) {
+    cl.rest.get("v1/viz/").on("complete", function(result) {
+      // Result must not be be an instance of error
+      assert(!(result instanceof Error));
+      done();
+    });
   });
 
   it('must validate a JSON schema', function () {
