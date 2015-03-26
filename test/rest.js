6fe30213212a6d4ff311717b8982717c3c3fa1e0
pirhoo <pierre.romera@gmail.com>
2015-03-26 18:41:57 +0100

    Added layers shortcuts

--- test/rest.js
+++ test/rest.js
@@ -28,4 +28,32 @@ describe('CartoDB REST client', function () {
     });
   });
 
+  it('must fetch layers', function (done) {
+    // Get layer from page 1
+    cl.rest.layers(1).on("complete", function() {
+      done();
+    });
+  });
+
+  it('must fetch layers from page 2', function (done) {
+    // Get layer from page 1
+    cl.rest.layers(1).on("complete", function(result) {
+      // We must have at least 2 visualizations
+      assert(result.total_entries >= 2, 'Unable to perform the test with less than 2 visualizations.');
+      // Save the id of the first visualization for later
+      var first_id = result.visualizations[0].id;
+      // To be sure that there is a second page, we ask less than
+      // total number of entries per_page
+      cl.rest.layers(2, ~~(result.total_entries/2) ).on("complete", function(result) {
+        // The API must return at least one visualization
+        assert(result.visualizations.length > 0, 'Second page must contain layers.');
+        // The first visualization of the second page must be different
+        // from the one in the first page
+        assert(first_id !== result.visualizations[0].id, 'The second page contains the same layers than the first one.');
+        done();
+      });
+
+    });
+  });
+
 });
